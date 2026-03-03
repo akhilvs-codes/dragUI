84b58b03967ee19f2958415e01dc42852a5745c1
akhilvskannan77@gmail.com <akhilvskannan77@gmail.com>
2026-03-03 19:32:43 +0530

    feat(ui):create a draggable table

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,83 @@
-import Image from "next/image";
+
+"use client"
+
+import { useRef, useState } from "react";
 
 export default function Home() {
+
+  let dragId = useRef(0)
+  const dragOverId = useRef(0)
+  let arr = [{
+    name: "akhil",
+    email: "eamilnwe",
+    place: "ee"
+  },
+  {
+    name: "ads",
+    email: "eamilnwe",
+    place: "ee"
+  }
+    , {
+    name: "akhileee",
+    email: "eamilnwe",
+    place: "ee"
+  }]
+
+  const [data, setData] = useState(arr)
+
+  let enterDrag = (index:number) => {
+
+    dragId.current =index
+    console.log(dragId.current);
+    
+  }
+
+  let startDrag = (index:number) => {
+    dragOverId.current = index
+  }
+
+  let drop = () => {
+
+    const items = [...data]
+    const dragItem = items[dragId.current]
+    items.splice(dragId.current, 1)
+    items.splice(dragOverId.current, 0, dragItem)
+    dragId.current = 0
+    dragOverId.current = 0
+    console.log(items);
+    
+    setData(items)
+
+  }
+
   return (
     <>
 
-      <div>
+      <div className="flex justify-center h-screen items-center">
 
-        <table>
+        <table className="w-2xl" >
           <thead>
-            <tr>
-
+            <tr className="text-left p-3">
+              <th>name</th>
+              <th>age</th>
+              <th>palce</th>
             </tr>
           </thead>
           <tbody>
-          <tr>
-            
-          </tr>
+            {
+              data.map((tr,idx) => (
+                <tr draggable key={idx} className="text-left text-red-300 border-2 m-1 p-1 mt-16"
+                  onDragStart={(e) => startDrag(idx)}
+                  onDragEnter={(e) => enterDrag(idx)}
+                  onDragEnd={(e) => drop()
+                  }>
+                  <td className="p-1">{tr.name}</td>
+                  <td>{tr.email}</td>
+                  <td>{tr.place}</td>
+                </tr>
+              ))
+            }
+
 
           </tbody>
         </table>
