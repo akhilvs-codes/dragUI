3a7772884fb6a10e42ea7ae4132053c1424a362b
akhilvskannan77@gmail.com <akhilvskannan77@gmail.com>
2026-03-03 18:46:42 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+
+      <div>
+
+        <table>
+          <thead>
+            <tr>
+
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+            
+          </tr>
+
+          </tbody>
+        </table>
+
+
+      </div>
+
+    </>
+  );
+}
